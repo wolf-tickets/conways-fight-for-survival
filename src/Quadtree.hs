a8ea03579996c660e149b41e974ca3c66a389aad
Tristan <tsmackinlay@gmail.com>
2018-10-13 14:17:32 -0700

    Fixed cabal file -Added a library build target, executable build target now lists other-modules correctly.

--- src/Quadtree.hs
+++ src/Quadtree.hs
@@ -1,9 +1,12 @@
 module Quadtree where
 
-data Quadtree t = Atom t |
+data Quadtree t = Cell t |
   Quadtree {
     ul :: Quadtree t,
     ur :: Quadtree t,
     ll :: Quadtree t,
     lr :: Quadtree t
 }
+
+square :: Quadtree [Char]
+square = Quadtree (Cell "ul") (Cell "ur") (Cell "ll") (Cell "lr")
