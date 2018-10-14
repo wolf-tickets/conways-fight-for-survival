020243845f957c0e45d37a7d8d51f842162e2ef6
Tristan <tsmackinlay@gmail.com>
2018-10-13 20:26:49 -0700

    Added some utility functions to Quadtree.hs

--- src/Quadtree.hs
+++ src/Quadtree.hs
@@ -1,12 +1,37 @@
 module Quadtree where
 
-data Quadtree t = Cell t |
-  Quadtree {
-    ul :: Quadtree t,
-    ur :: Quadtree t,
-    ll :: Quadtree t,
-    lr :: Quadtree t
-}
+data Quadtree t
+  = Cell t
+  | Quadtree { ul :: Quadtree t
+             , ur :: Quadtree t
+             , ll :: Quadtree t
+             , lr :: Quadtree t }
+  deriving (Show)
 
-square :: Quadtree [Char]
-square = Quadtree (Cell "ul") (Cell "ur") (Cell "ll") (Cell "lr")
+-- depth qt => returns the depth of the Quadtree; depth of a cell is 0
+depth :: Quadtree t -> Int
+depth (Cell v) = 0
+depth (Quadtree ul ur ll lr) = (1 + maximum (map depth [(ul), (ur), (ll), (lr)]))
+
+-- produce Quadtree t from list of four Quadtree t
+qtFromList :: [Quadtree t] -> Quadtree t
+qtFromList [ul, ur, ll, lr] = Quadtree ul ur ll lr
+
+-- Get list of subtrees from Quadtree
+qtToList :: (Quadtree t) -> [Quadtree t]
+qtToList (Quadtree ul ur ll lr) = [ul, ur, ll, lr]
+
+
+--prettyPrint -- Produces a nice string representation
+--                of a given Quadtree, works for Quadtree Char
+--                with subtrees of equal depth.
+prettyPrint :: Quadtree Char -> String
+prettyPrint (Cell val) = [val]
+prettyPrint (Quadtree ul ur ll lr) =
+  foldr
+    (++)
+    []
+    [ unlines $ zipWith (++) (lines $ prettyPrint ul) (lines $ prettyPrint ur)
+    , unlines $ zipWith (++) (lines $ prettyPrint ll) (lines $ prettyPrint lr)
+    ]
+
